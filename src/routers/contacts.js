b04caa48d2a2f591f640196930f5f0168ae8b8c7
ValentynKravchuk <167019009+ValentynKravchuk@users.noreply.github.com>
2025-01-23 17:11:24 +0100

    update import

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,5 +1,5 @@
 import {
-  createContactsController,
+  createContactController,
   deleteContactController,
   getContactByIdController,
   getContactsController,
@@ -14,7 +14,7 @@ const router = Router();
 router.get('/contacts', ctrlWrapper(getContactsController));
 
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts', ctrlWrapper(createContactsController));
+router.post('/contacts', ctrlWrapper(createContactController));
 router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 router.put('/contacts/:contactId', ctrlWrapper(upsertContactController));
