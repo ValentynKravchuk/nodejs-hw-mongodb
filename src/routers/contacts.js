ef35904512240726c16be731e59c143dd2ca0f0a
ValentynKravchuk <167019009+ValentynKravchuk@users.noreply.github.com>
2025-01-30 16:41:33 +0100

    step 1 and step 2

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,13 +8,31 @@ import {
   upsertContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactScheme,
+  updateContactScheme,
+} from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 router.get('/contacts', ctrlWrapper(getContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts', ctrlWrapper(createContactController));
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.get(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(getContactByIdController),
+);
+router.post(
+  '/contacts',
+  validateBody(createContactScheme),
+  ctrlWrapper(createContactController),
+);
+router.patch(
+  '/contacts/:contactId',
+  validateBody(updateContactScheme),
+  ctrlWrapper(patchContactController),
+);
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 router.put('/contacts/:contactId', ctrlWrapper(upsertContactController));
 export default router;
