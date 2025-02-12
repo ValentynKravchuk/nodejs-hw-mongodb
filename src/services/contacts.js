5c10d26bcd5b0f275607a1d92995a4046ed17c7c
ValentynKravchuk <167019009+ValentynKravchuk@users.noreply.github.com>
2025-02-12 20:49:37 +0100

    update

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -13,7 +13,7 @@ export const getAllContacts = async ({
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
-  const contactsQuery = ContactsCollection.find();
+  const contactsQuery = ContactsCollection.find({ userId });
 
   const validTypes = ['work', 'home', 'personal'];
   if (validTypes.includes(filter.type)) {
