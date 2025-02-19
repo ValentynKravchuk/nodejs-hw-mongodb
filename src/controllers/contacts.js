90e723f6c8387669e455fa01a81f38e5814ac7a0
ValentynKravchuk <167019009+ValentynKravchuk@users.noreply.github.com>
2025-02-19 01:05:11 +0100

    fix

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -47,7 +47,24 @@ export const getContactByIdController = async (req, res) => {
   });
 };
 export const createContactController = async (req, res) => {
-  const contact = await createContact(req.body, req.user._id);
+  const photo = req.file;
+
+  let photoUrl;
+
+  if (photo) {
+    if (getEnvVar('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
+  const contact = await createContact(
+    {
+      ...req.body,
+      photo: photoUrl,
+    },
+    req.user._id,
+  );
   res.status(201).json({
     status: 201,
     message: 'Successfully created a contact!',
